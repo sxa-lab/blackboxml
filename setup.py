ac28645361e9294a2e0cf87880168ebddc73046e
Stuart Asiimwe <stuartgabriel@ymail.com>
2026-03-11 00:46:08 +0900

    update setup.py for v0.5.0

--- setup.py
+++ setup.py
@@ -2,29 +2,34 @@ from setuptools import setup, find_packages
 
 setup(
     name='blackboxml',
-    version='0.2.0',
+    version='0.5.0',
     author='Stuart Asiimwe',
     author_email='stuartgabriel@ymail.com',
-    description="Automatically logs Keras training metrics.",
+    description='Lightweight, framework-agnostic ML experiment tracker.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/stuartasiimwe7/blackboxml',
     packages=find_packages(),
-     install_requires=[
-        'tensorflow>=2.0.0',
-        'scikit-learn',
+    install_requires=[
         'matplotlib',
-        'numpy',
-        'pandas',
+        'click',
     ],
+    extras_require={
+        'keras': ['tensorflow>=2.0.0'],
+    },
+    entry_points={
+        'console_scripts': [
+            'bbml=blackboxml.cli:main',
+        ],
+    },
+    python_requires='>=3.10',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
-    
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
 )
